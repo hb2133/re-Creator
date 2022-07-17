c05b4128a68b02bc973a92602f718f64172dedff
20181248 이현승 <53048916+CaptainLee-P@users.noreply.github.com>
2022-07-17 18:42:38 +0900

    sibal

--- Source/test_Project_3/test_Project_3.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "test_Project_3.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, test_Project_3, "test_Project_3" );
- 
