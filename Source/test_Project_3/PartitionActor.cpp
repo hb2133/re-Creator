f7440c9fa44e35e1b726fda5fdb786aa61ac3fc5
2018김수종 <tnwhd6268@naver.com>
2022-07-20 18:08:11 +0900

    07/20 v2

--- Source/test_Project_3/PartitionActor.cpp
+++ Source/test_Project_3/PartitionActor.cpp
@@ -57,3 +57,62 @@ void APartitionActor::Tick(float DeltaTime)
 	}*/
 	
 }
+
+void APartitionActor::setRoom()
+{
+	if (roomDir.up.nextMap != nullptr) {
+		roomTriggerDir.up.nextMap = roomDir.up.nextMap;
+	}
+	if (roomDir.down.nextMap != nullptr) {
+		roomTriggerDir.down.nextMap = roomDir.down.nextMap;
+	}
+	if (roomDir.right.nextMap != nullptr) {
+		roomTriggerDir.right.nextMap = roomDir.right.nextMap;
+	}
+	if (roomDir.left.nextMap != nullptr) {
+		roomTriggerDir.left.nextMap = roomDir.left.nextMap;
+	}
+}
+
+FString APartitionActor::ContainName(AActor* inputActor
+	,const FString& up, const FString& down, const FString& right, const FString& left)
+{
+	if (inputActor->GetName().Contains(up)) {
+		return const_cast<FString&>(up);
+	}
+	else if (inputActor->GetName().Contains(down)) {
+		return const_cast<FString&>(down);
+	}
+	else if (inputActor->GetName().Contains(right)) {
+		return const_cast<FString&>(right);
+	}
+	else if (inputActor->GetName().Contains(left)) {
+		return const_cast<FString&>(left);
+	}
+
+	return FString();
+}
+
+FDirect& APartitionActor::dirAdd(AActor* element, EDIRECT dir)
+{
+	switch (dir) {
+	case EDIRECT::UP:
+		roomTriggerDir.up.nextMapTrigger = element;
+		roomTriggerDir.up.eDir = EDIRECT::UP;
+		break;
+	case EDIRECT::DOWN:
+		roomTriggerDir.down.nextMapTrigger = element;
+		roomTriggerDir.down.eDir = EDIRECT::DOWN;
+		break;
+	case EDIRECT::RIGHT:
+		roomTriggerDir.right.nextMapTrigger = element;
+		roomTriggerDir.right.eDir = EDIRECT::RIGHT;
+		break;
+	case EDIRECT::LEFT:
+		roomTriggerDir.left.nextMapTrigger = element;
+		roomTriggerDir.left.eDir = EDIRECT::LEFT;
+		break;
+	}
+	return roomTriggerDir;
+}
+
--- Source/test_Project_3/PartitionActor.h
+++ Source/test_Project_3/PartitionActor.h
@@ -7,19 +7,52 @@
 #include "PartitionActor.generated.h"
 
 
+UENUM(BlueprintType)
+enum class EDIRECT : uint8 {
+	UP UMETA(DisplayName = "Up"),
+	DOWN UMETA(DisplayName = "Down"),
+	RIGHT UMETA(DisplayName = "Right"),
+	LEFT UMETA(DisplayName = "Left"),
+};
+
+UENUM(BlueprintType)
+enum class EROOMTYPE : uint8 {
+	MONSTER UMETA(DisplayName = "Monster"),
+	SHOP UMETA(DisplayName = "Shop"),
+	BOSS UMETA(DisplayName = "Boss"),
+	
+};
+
+USTRUCT(Atomic, BlueprintType)
+struct FRoomNode {
+
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
+		AActor* nextMap;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
+		AActor* nextMapTrigger;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
+		EDIRECT eDir;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
+		EROOMTYPE roomType;
+
+};
+
 USTRUCT(Atomic, BlueprintType)
 struct FDirect {
 
 	GENERATED_BODY()
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
-		AActor* up;
+		FRoomNode up;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
-		AActor* down;
+		FRoomNode down;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
-		AActor* right;
+		FRoomNode right;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
-		AActor* left;
+		FRoomNode left;
+	
 };
 
 UCLASS()
@@ -35,9 +68,20 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+		void setRoom();
+
+	UFUNCTION(BlueprintCallable)
+		FString ContainName(AActor* inputActor
+			,const FString& up, const FString& down, const FString& right, const FString& left);
+	UFUNCTION(BlueprintCallable)
+		FDirect& dirAdd(AActor* element, EDIRECT dir);
+		
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
 		AActor * pivotActor;
@@ -47,6 +91,8 @@ public:
 		FDirect roomDir;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Map")
 		FDirect roomTriggerDir;
+
+
 };
 
 
